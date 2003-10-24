480c8438b177486a2de5b970a573f813b954da3c
uedalab <lang@ueda.info.waseda.ac.jp>
2003-10-24 09:59:41 +0000

    javadoc,Env

--- src/runtime/Env.java
+++ src/runtime/Env.java
@@ -12,15 +12,15 @@ public final class Env {
 	public static Machine machine = new Machine();
 	public static Membrane rootMembrane = new Membrane(machine);
 	
-	// General dumper
+	/** General dumper */
 	public static void p(Object o) {
 		System.out.println(o);
 	}
-	// Called method
+	/** Debug output when method called */
 	public static void c(Object o) {
 		p(">>> "+o);
 	}
-	// New object
+	/** Debug output when new object */
 	public static void n(Object o) {
 		p(">>> new "+o);
 	}
