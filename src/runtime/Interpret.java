3ec6ccde7c3e2ad846c9e7fe8217fb69de1a97a5
uedalab <lang@ueda.info.waseda.ac.jp>
2003-11-02 10:14:34 +0000

    NAKAJIMA: created interper.java

---
+++ src/runtime/Interpret.java
@@ -0,0 +1,17 @@
+package runtime;
+
+
+/*
+ * LMNtalのインタプリタ。
+ * @author NAKAJIMA Motomu
+ * 
+ * @see 参照クラス名
+ */
+
+public class Interpret {
+    /** フィールドの説明 **/
+    //フィールドの定義 
+    
+    
+
+}
