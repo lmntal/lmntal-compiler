94ac2af3d15a776d68a0b44fadd3cb285edc7e1f
n <n@ueda.info.waseda.ac.jp>
2003-12-12 11:56:13 +0000

    *** empty log message ***

--- src/compile/structure/RuleStructure.java
+++ src/compile/structure/RuleStructure.java
@@ -4,23 +4,16 @@ package compile.structure;
  * ソースコード中のルールの構造を表すクラス
  */
 public final class RuleStructure {
-	/**
-	 * 所属膜。コンパイル時につかうので予めセットしてください
-	 */
+	/** 所属膜。コンパイル時につかうので予めセットしてください */
 	public Membrane parent;
-	/**
-	 * Headルールを格納する膜
-	 */
+
+	/** Headを格納する膜 */
 	public Membrane leftMem = new Membrane(null);
 	
-	/**
-	 * Bodyルールを格納する膜
-	 */
+	/** Bodyを格納する膜 */
 	public Membrane rightMem = new Membrane(null);
 	
-	/**
-	 * ガードルールを格納する膜
-	 */
+	/** ガードを格納する膜 */
 	public Membrane guardMem = new Membrane(null);
 	
 	public String toString() {
