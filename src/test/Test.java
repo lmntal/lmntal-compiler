f338921fa5d1eaf4ecb249dcbba264dbeac0e963
hara <hara@ueda.info.waseda.ac.jp>
2004-01-26 06:08:41 +0000

    t

---
+++ src/test/Test.java
@@ -0,0 +1,23 @@
+/*
+ * ºîÀ®Æü: 2004/01/26
+ *
+ */
+package test;
+
+
+import runtime.Env;
+
+/**
+ * @author pa
+ *
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		java.util.Enumeration e = System.getProperties().keys();
+		while(e.hasMoreElements()) {
+			String o = (String)e.nextElement(); 
+			Env.p(o+"  =>  "+System.getProperty(o));
+		}
+	}
+}
