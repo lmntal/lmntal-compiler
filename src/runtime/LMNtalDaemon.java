8c8754e9a79a0d661a296dfadcf1c35af6d8866c
nakajima <nakajima@ueda.info.waseda.ac.jp>
2004-04-30 08:50:37 +0000

    added LMNtalDaemon.java

---
+++ src/runtime/LMNtalDaemon.java
@@ -0,0 +1,16 @@
+package runtime;
+
+//import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.net.Socket;
+
+//TODO 実装
+
+/**
+ * 物理的な計算機の境界にあって、LMNtalRuntimeインスタンスとリモートノードの対応表を保持する。
+ * @author nakajima
+ *
+ */
+public class LMNtalDaemon{
+}
