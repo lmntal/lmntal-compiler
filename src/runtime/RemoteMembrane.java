b450bb35d0f6de2314dce47e7838437c081129e3
nakajima <nakajima@ueda.info.waseda.ac.jp>
2004-10-25 04:53:12 +0000

    udpateCache()

--- src/runtime/RemoteMembrane.java
+++ src/runtime/RemoteMembrane.java
@@ -344,6 +344,7 @@ public final class RemoteMembrane extends AbstractMembrane {
 						continue;
 					}
 					r = (RemoteLMNtalRuntime)ar;
+					if(true)System.out.println("RemoteMembrane.updateCache(): cast À®¸ù");
 				}
 				//
 				RemoteTask t;
