c49d68e84a9d1f76207856247c94b882f29c7062
nakajima <nakajima@ueda.info.waseda.ac.jp>
2004-11-01 09:04:07 +0000

    println

--- src/runtime/FrontEnd.java
+++ src/runtime/FrontEnd.java
@@ -322,7 +322,8 @@ public class FrontEnd {
 			}
 			if (Env.gui != null)  Env.gui = null;
 			LMNtalRuntimeManager.terminateAll();
-			LMNtalRuntimeManager.disconnectFromDaemon();
+			if(true) System.out.println("FrontEnd: terminateAll() finished!");
+			//LMNtalRuntimeManager.disconnectFromDaemon();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
