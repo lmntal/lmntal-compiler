b6ce99bbae0ef1a47ac7b91239e3d77ca75aab0b
nakajima <nakajima@ueda.info.waseda.ac.jp>
2004-11-12 09:13:57 +0000

    terminateAll -> terminateAllThreaded

--- src/test/GUI/LMNtalFrame.java
+++ src/test/GUI/LMNtalFrame.java
@@ -39,7 +39,7 @@ public class LMNtalFrame extends JFrame implements KeyListener {
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				running = busy = false;
-				runtime.LMNtalRuntimeManager.terminateAll();	// 追加 n-kato 2004-10-30
+				runtime.LMNtalRuntimeManager.terminateAllThreaded();	// 追加 n-kato 2004-10-30
 			}
 		});
 		initComponents();
