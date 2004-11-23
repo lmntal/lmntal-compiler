0d30974ff012e254567062533ed30274bf09585d
nakajima <nakajima@ueda.info.waseda.ac.jp>
2004-11-23 04:06:52 +0000

    Env

--- src/daemon/IDConverter.java
+++ src/daemon/IDConverter.java
@@ -36,8 +36,7 @@ public class IDConverter {
 
 	/** 指定された膜を表に登録する */
 	public static void registerGlobalMembrane(String globalMemID, AbstractMembrane mem) {
-		boolean DEBUG = false;
-		if(DEBUG) System.out.println("IDConverter.registerGlobalMembrane(" + globalMemID + ", " + mem.toString() + ")"); //todo use Env
+		if(Env.debugDaemon > 0) System.out.println("IDConverter.registerGlobalMembrane(" + globalMemID + ", " + mem.toString() + ")"); //todo use Env
 		memTable.put(globalMemID, mem);
 	}
 	/** 指定された膜を表から削除する */
