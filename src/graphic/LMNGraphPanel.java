9d179b2b4f3bbd4f42124c272af20801fb4f00ba
nakano <nakano@ueda.info.waseda.ac.jp>
2005-12-01 17:38:02 +0000

    *** empty log message ***

--- src/graphic/LMNGraphPanel.java
+++ src/graphic/LMNGraphPanel.java
@@ -2,8 +2,7 @@ package graphic;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.util.Iterator;
-import java.util.LinkedList;
+import java.util.*;
 
 import javax.swing.*;
 import javax.swing.border.*;
