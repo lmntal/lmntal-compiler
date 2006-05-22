7f224652fda7dcc78e32be3ceaca2e1e956e2690
kudo <kudo@ueda.info.waseda.ac.jp>
2006-05-22 14:31:59 +0000

    lmntal system checker script

---
+++ tools/system_check/check.pl
@@ -0,0 +1,56 @@
+#!/usr/bin/perl
+
+######################################
+#                                    #
+# Java LMNtal system checker on Perl #
+#                                    #
+######################################
+# AUTHOR : kudo
+# LAST UPDATE : 2006/05/22
+
+# This is LMNtal system checker.
+
+open(PWD, "dirname $0 |");
+$pwd = <PWD>;
+$pwd =~ s/\n//;
+
+$lmntal_runtime = $pwd . "/../../bin/lmntal";
+
+open(INPUTFILE, $pwd . "/testdata.lmn");  # input file
+@file = <INPUTFILE>;
+
+for($i=0;$i<=$#file;$i++){
+	$file[$i] =~ s/\n//;                                # delete return code
+	@data = split /%/, $file[$i];
+	$program = $data[0];                                # input data
+	$goal = $data[1];                                   # right answer
+	$flg = $data[2];                                    # ok / ng
+	unless($program && $goal){next;}                    # skip comment
+	unless($flg){$flg = "ok"}
+
+	$program =~ s/\$/\\\$/g;
+	$run = $lmntal_runtime . " -e \"" . $program . "\"";
+	$output = `$run`;
+
+	$output =~ s/\n//;
+	$output =~ s/@[0-9]+/()/g;
+	print (($i+1) . " : ");
+	$check_program = "{" . $output . "}, ({" . $goal . "}/:-ok)";
+	$check_run = $lmntal_runtime . " -e \"" . $check_program . "\"";
+	$checked = `$check_run`;
+
+	$result = index ($checked, "ok");
+	if($flg == "ok" && $result == 0){
+		print "ok";
+	}elsif($flg == "ng" && $result != 0){
+		print "ok";
+	}else{
+		print "ng : ";
+		print $program;
+		print " ==> ";
+		print $output;
+		print " <=> " . $goal;
+	}
+	print "\n";
+}
+close(IN);
