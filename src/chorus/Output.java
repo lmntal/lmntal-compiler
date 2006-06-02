3d1ce9ba39034fd46df8dc52dc4bbe8e0b74f50b
nakano <nakano@ueda.info.waseda.ac.jp>
2006-06-02 12:59:58 +0000

    *** empty log message ***

--- src/chorus/Output.java
+++ src/chorus/Output.java
@@ -178,7 +178,7 @@ public final class Output{
 						msg = atomID +
 								"," +
 								((Integer)atomMap.get(atom2)).intValue() +
-								",2" +
+								",1" +
 								"\n";
 						bw.write(msg);
 					}
