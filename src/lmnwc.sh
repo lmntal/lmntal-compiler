785023b325ea8ee9a7902a84f191f8f0010dd767
inui <inui@ueda.info.waseda.ac.jp>
2006-06-06 09:15:53 +0000

    *** empty log message ***

--- src/lmnwc.sh
+++ src/lmnwc.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-wc `find $1 | grep "\.java$"`
+echo -n "total (except test)"
+wc `find $1 | grep "\.java$" | grep -v test` -l | tail -1
+
+echo -n "  compile"
+wc compile/*.java compile/parser/*.java compile/structure/*.java  -l | tail -1
+
+echo -n "  runtime"
+wc runtime/*.java -l | tail -1 | sed 's/\[ \]\+/ /g'
