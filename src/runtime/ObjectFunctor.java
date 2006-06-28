d757e62f80716e513fa49f01be2d4b061f0c3071
inui <inui@ueda.info.waseda.ac.jp>
2006-06-28 05:31:17 +0000

    subclass

--- src/runtime/ObjectFunctor.java
+++ src/runtime/ObjectFunctor.java
@@ -11,7 +11,21 @@ public class ObjectFunctor extends Functor {
 	public Object getObject() { return data; }
 	public Object getValue() { return data; }
 	public boolean equals(Object o) {
-		return o.getClass() == getClass() && data.equals(((ObjectFunctor)o).data);
+//		return o.getClass() == getClass() && data.equals(((ObjectFunctor)o).data);
+		
+		//2006.6.26 by inui
+		try {
+			return (o.getClass() == getClass()) && isSubclass(Class.forName(data.toString()), Class.forName(((ObjectFunctor)o).data.toString()));
+		} catch (ClassNotFoundException e) {
+			return false;
+		}
 	}
 	public String getName() { return data.toString(); }
+	
+	//c1がc2のサブクラスかどうか判定します 2006.6.26 by inui
+	private static boolean isSubclass(Class c1, Class c2) {
+		if (c1.equals(c2)) return true;
+		if (c1.equals(Object.class)) return false;
+		return isSubclass(c1.getSuperclass(), c2);
+	}
 }
