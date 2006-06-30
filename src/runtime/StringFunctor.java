a69684d0f04b18143cb9f3bec117ead9f8abc044
inui <inui@ueda.info.waseda.ac.jp>
2006-06-30 07:25:33 +0000

    extends ObjectFunctor => extends Functor

--- src/runtime/StringFunctor.java
+++ src/runtime/StringFunctor.java
@@ -6,17 +6,18 @@ import util.Util;
  * todo inlineの扱いがおかしいので何とかする
  * @author n-kato
  * @see ObjectFunctor#getName() */
-public class StringFunctor extends ObjectFunctor {
-	public StringFunctor(String data) { super(data); }
+public class StringFunctor extends Functor {
+	public StringFunctor(String data) { super(data, 1);}
 	public String getQuotedAtomName() { return getStringLiteralText(getName()); }
 	public String getQuotedFunctorName() { return getQuotedAtomName(); }
 	public String stringValue() {return getName();}
 	public String toString() {
-		return Util.quoteString((String)data, '"') + "_" + getArity();
+		return Util.quoteString(getName(), '"') + "_" + getArity();
 	}
 	// 2006/06/28 by kudo
 	public boolean equals(Object o){
 		if(!(o instanceof StringFunctor))return false;
-		return ((StringFunctor)o).data.equals(this.data);
+		//コンストラクタでinternしているので、==で比較できる。
+		return (getName() == ((StringFunctor)o).getName());//2006.6.30 by inui
 	}
 }
