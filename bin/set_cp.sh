8741695da61e2258b252f58e3599149f49a15fab
inui <inui@ueda.info.waseda.ac.jp>
2006-07-15 13:50:57 +0000

    cygpath $LMNTAL_HOME

--- bin/set_cp.sh
+++ bin/set_cp.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 export LMNTAL_HOME=`dirname $0`/..
-if [ -x /usr/bin/cygpath ]; then
-	export LMNTAL_HOME=`cygpath -wp $LMNTAL_HOME`
-fi
 
 CLASSPATH=$LMNTAL_HOME/classes
 for f in $LMNTAL_HOME/lib/*.jar; do CLASSPATH=$CLASSPATH:$f; done
