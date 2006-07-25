5395bd6013b0cc84394c6ee75740a8cd08fa41b1
inui <inui@ueda.info.waseda.ac.jp>
2006-07-25 14:50:01 +0000

    *** empty log message ***

--- tools/find_module_use.sh
+++ tools/find_module_use.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
-# 指定されたモジュールが使われているプログラムを sample から探します
+# 指定されたモジュールが使われているプログラムを
+# sample, lib/src, lib/public から探します
+#
 # by inui
 #
 # 使用例
@@ -19,6 +21,8 @@ LMNTAL_HOME=~/workspace/lmntal
 # そういうモジュールがあるかどうか調べる
 if [ -e $LMNTAL_HOME/lib/public/$1.lmn -o -e $LMNTAL_HOME/lib/src/$1.lmn ]; then
 	grep "\\b$1\\." `find $LMNTAL_HOME/sample -name *.lmn` | cut -d: -f1 | uniq
+	grep "\\b$1\\." `find $LMNTAL_HOME/lib/public -name *.lmn` | cut -d: -f1 | uniq
+	grep "\\b$1\\." `find $LMNTAL_HOME/lib/src -name *.lmn` | cut -d: -f1 | uniq
 else
 	echo $1: module not found.
 	exit
