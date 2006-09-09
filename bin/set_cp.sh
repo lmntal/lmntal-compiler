1ae6004d8a451f7b380e702c48e397a87298881e
inui <inui@ueda.info.waseda.ac.jp>
2006-09-09 06:50:07 +0000

    *** empty log message ***

--- bin/set_cp.sh
+++ bin/set_cp.sh
@@ -4,6 +4,5 @@ export LMNTAL_HOME=`dirname $0`/..
 
 CLASSPATH=$LMNTAL_HOME/classes
 for f in $LMNTAL_HOME/lib/*.jar; do CLASSPATH=$CLASSPATH:$f; done
-for f in $LMNTAL_HOME/tools/Retrotranslator-1.0.8-bin/*.jar; do CLASSPATH=$CLASSPATH:$f; done
 
 export CLASSPATH
