943bb12e57907a4b55b5deccc173c5a8bfd0ae3a
inui <inui@ueda.info.waseda.ac.jp>
2006-09-27 07:26:46 +0000

    *** empty log message ***

--- tools/Retrotranslator-1.0.8-bin/run.sh
+++ tools/Retrotranslator-1.0.8-bin/run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-LMNTAL_HOME=~/workspace/lmntal
+export LMNTAL_HOME=`dirname $0`/../..
+export RETROTRANSLATOR=$LMNTAL_HOME/tools/Retrotranslator-1.0.8-bin
 
-java -jar retrotranslator-transformer-1.0.8.jar -srcdir $LMNTAL_HOME/classes
+for f in $RETROTRANSLATOR/*.jar; do CLASSPATH=$CLASSPATH:$f; done
+export CLASSPATH
+
+java -jar $RETROTRANSLATOR/retrotranslator-transformer-1.0.8.jar -srcdir $LMNTAL_HOME/classes
