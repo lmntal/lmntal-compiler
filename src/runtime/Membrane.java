67896efa748bc8282f17c696236161e70e45156d
kei <kei@ueda.info.waseda.ac.jp>
2006-10-02 05:42:53 +0000

    *** empty log message ***

--- src/runtime/Membrane.java
+++ src/runtime/Membrane.java
@@ -324,10 +324,10 @@ public final class Membrane extends QueuedEntity {
 	 * 走査済みの印をつけておく．
 	 * 走査完了後，未到達の引数があれば-1を返す．
 	 * 
-	 * @param avoList 基底項プロセスに出てきてはいけないリンクのList
+	 * @param avoSet 基底項プロセスに出てきてはいけないリンクのSet
 	 * @return 基底項プロセスを構成するアトム数
 	 */
-	public static int isGround(List/*<Link>*/ links, Set avoSet){
+	public int isGround(List/*<Link>*/ links, Set avoSet){
 		Set srcSet = new HashSet(); // 走査済みアトム
 		java.util.Stack s = new java.util.Stack(); //リンクを積むスタック
 		s.push(links.get(0)); // 第1引数から走査する
@@ -338,7 +338,7 @@ public final class Membrane extends QueuedEntity {
 			Link l = (Link)s.pop();
 			Atom a = l.getAtom();
 			if(srcSet.contains(a))continue; //既に辿ったアトム
-			if(avoSet.contains(l.getBuddy()))return -1; //出現してはいけないリンク
+			if(avoSet.contains(l))return -1; //出現してはいけないリンク
 			int argi = links.indexOf(l.getBuddy());
 			if(argi >= 0){ // 基底項プロセスの引数に到達
 				exists[argi] = true;
@@ -370,7 +370,7 @@ public final class Membrane extends QueuedEntity {
 	 * @param srcLink 比較対象のリンク
 	 * @return
 	 */
-	public static boolean eqGround(List/*<Link>*/ srclinks, List/*<Link>*/ dstlinks){
+	public boolean eqGround(List/*<Link>*/ srclinks, List/*<Link>*/ dstlinks){
 		Map map = new HashMap(); //比較元アトムから比較先アトムへのマップ
 		java.util.Stack s1 = new java.util.Stack();  //比較元リンクを入れるスタック
 		java.util.Stack s2 = new java.util.Stack();  //比較先リンクを入れるスタック
@@ -407,7 +407,7 @@ public final class Membrane extends QueuedEntity {
 	 * つまり，このメソッドでは考慮する必要はない．
 	 * 
 	 * @param srcGround コピー元の基底項プロセス の根のリスト
-	 * @return 2要素のリスト ( 第一要素 : コピー先の基底項プロセス の根のリスト, 第二要素 : コピー元のアトムからコピー先のアトムへのマップ)
+	 * @return コピー先の基底項プロセス の根のリスト
 	 */
 	public List/*<Link>*/ copyGroundFrom(List/*<Link>*/ srclinks){//Link srcGround){
 		java.util.Stack s = new java.util.Stack();
@@ -445,10 +445,7 @@ public final class Membrane extends QueuedEntity {
 			Link srclink = (Link)srclinks.get(i);
 			dstlinks.add( new Link(((Atom)map.get(srclink.getAtom())),srclink.getPos()));
 		}
-		List ret_list = new ArrayList();
-		ret_list.add(dstlinks);
-		ret_list.add(map);
-		return ret_list;
+		return dstlinks;
 //		return new Link(((Atom)map.get(srcGround.getAtom())),srcGround.getPos());
 	}
 	
@@ -619,15 +616,13 @@ public final class Membrane extends QueuedEntity {
 		relinkAtomArgs(atom1, pos1, atom2, pos2);
 	}
 	/** link1の指すアトム引数とlink2の指すアトム引数の間に、双方向のリンクを張る。
-	 * <p>実行後link1およびlink2自身は無効なリンクオブジェクトになるため、参照を使用してはならない。
-	 * */
+	 * <p>実行後link1およびlink2自身は無効なリンクオブジェクトになるため、参照を使用してはならない。*/
 	public void unifyLinkBuddies(Link link1, Link link2) {
 		//link1.getBuddy().set(link2);
 		//link2.getBuddy().set(link1);
 		link1.getAtom().args[link1.getPos()] = link2;
 		link2.getAtom().args[link2.getPos()] = link1;
 	}
-	
 	/** atom1の第pos1引数と、リンクlink2のリンク先を接続する。
 	 * <p>link2は再利用されるため、実行後link2の参照を使用してはならない。*/
 	public void inheritLink(Atom atom1, int pos1, Link link2) {
@@ -1430,8 +1425,6 @@ public final class Membrane extends QueuedEntity {
 
 	/** 活性化する。
 	 * <p>
-	 * <strike>すでにスタックに積まれていれば何もしない。</strike>
-	 * スタックに積まれていないならば、
 	 * <dl>
 	 * <dt><b>ルート膜の場合</b>:<dd>
 	 * このタスクの仮の実行膜スタックの唯一の要素として積む。
@@ -1457,7 +1450,7 @@ public final class Membrane extends QueuedEntity {
 				s.push(this);
 			} else {
 				//方法7では、この膜をロックしていれば非同期にpushされている事はない。
-				//方法8では、下2行をsynchronized(memStack)内に入れる。
+				//方法8では、下2行をsynchr`onized(memStack)内に入れる。
 				if (!isQueued())
 					s.push(this);
 			}
@@ -1474,7 +1467,6 @@ public final class Membrane extends QueuedEntity {
 		//方法8では、この下を全てsynchronized(memStack)内に入れる。
 		if (isQueued())
 			return stack;
-
 		if (isRoot()) {
 			// ASSERT(t.bufferedStack.isEmpty());
 			t.bufferedStack.push(this);
@@ -1493,8 +1485,8 @@ public final class Membrane extends QueuedEntity {
 	/**
 	 * この膜のロック取得を試みる。
 	 * <p>ルールスレッドまたはdumperがこの膜のロックを取得するときに使用する。
-	 * <p>ルールスレッドは、ロック解放にはunlock()を使用する。
-	 * <p>dumperは、ロック解放にはquietUnlock()を使用する。
+	 * <p>ルールスレッドは、ロック解放にはunlock()を使用すること。
+	 * <p>dumperは、ロック解放にはquietUnlock()を使用すること。
 	 * <p>成功したら親膜のリモートを継承する。
 	 * したがってルールスレッドは、本膜をロックした場合ただちにリモートをnullに設定すること。
 	 * @return ロックの取得に成功したかどうか */
@@ -1513,7 +1505,6 @@ public final class Membrane extends QueuedEntity {
 	 * 失敗した場合、この膜を管理するタスクのルールスレッドに停止要求を送る。その後、
 	 * このタスクがシグナルを発行するのを待ってから、再びロック取得を試みることを繰り返す。
 	 * <p>ルールスレッド以外のスレッドがこの膜のロックを2つ目以降のロックとして取得するときに使用する。
-	 * <p>成功したら親膜のリモートを継承する。
 	 * <p>ロック解放にはunlock()を使用すること。
 	 * 親膜のロックはすでに取得している必要がある。
 	 * @return 常にtrue */
@@ -1597,7 +1588,8 @@ public final class Membrane extends QueuedEntity {
 	}
 
 	/** このロックした膜の全ての子孫の膜のロックを再帰的にブロッキングで取得する。
-	 * <p>ロック解放にはrecursiveUnlock()を使用する。
+	 * <p>プロセス文脈のコピーや破棄をするときに使われる。
+	 * <p>ロック解放にはrecursiveUnlock()を使用すること。
 	 * @return ロックの取得に成功したかどうか */
 	public boolean recursiveLock() {
 		Iterator it = memIterator();
