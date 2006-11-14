072725a326c67f7e42a0f742420526c3686fa2f6
inui <inui@ueda.info.waseda.ac.jp>
2006-11-14 06:13:21 +0000

    1.1.0 released!

--- tools/Retrotranslator-1.0.8-bin/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-export LMNTAL_HOME=`dirname $0`/../..
-export RETROTRANSLATOR=$LMNTAL_HOME/tools/Retrotranslator-1.0.8-bin
-
-for f in $RETROTRANSLATOR/*.jar; do CLASSPATH=$CLASSPATH:$f; done
-export CLASSPATH
-
-java -jar $RETROTRANSLATOR/retrotranslator-transformer-1.0.8.jar -srcdir $LMNTAL_HOME/classes
