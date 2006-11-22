f83a22133747da3da1323a4d29cb6a8b99239f5d
nakano <nakano@ueda.info.waseda.ac.jp>
2006-11-22 08:48:18 +0000

    *** empty log message ***

--- src/gui2/NodeFunction.java
+++ src/gui2/NodeFunction.java
@@ -1,8 +1,11 @@
 package gui2;
 
 import java.awt.geom.Point2D;
+import java.util.Iterator;
+import java.util.Map;
 
 import runtime.Atom;
+import runtime.Membrane;
 
 /**
  * アトムノード，膜ノードに関する操作
@@ -14,7 +17,7 @@ import runtime.Atom;
 public class NodeFunction {
 
 	///////////////////////////////////////////////////////////////////////////
-	/* ばね定数 */
+	/** ばね定数 */
 	final static
 	private double CONSTANT_SPRING = 0.01;
 	
@@ -28,20 +31,12 @@ public class NodeFunction {
 	public void calcSpring(Node node){
 		if(!Atom.class.isInstance(node.getObject())){
 			return;
-		} else {
-			calcSpring_Atom(node);
 		}
-	}
-
-	/**
-	 * ばねモデルの計算(Atom用)
-	 *
-	 */
-	static
-	private void calcSpring_Atom(Node node){
+		
 		Atom atom = (Atom)node.getObject();
 		// 表示されているNodeを取得する
 		Node sourceNode = LinkSet.getNode(atom);
+		if(null == sourceNode){ return; }
 		Point2D myPoint = sourceNode.getCenterPoint();
 		for(int i = 0; i < atom.getEdgeCount() ; i++){
 			Atom nthAtom = atom.getNthAtom(i);
@@ -56,8 +51,8 @@ public class NodeFunction {
 			}
 
 			double distance =
-		    Point2D.distance(myPoint.getX(), myPoint.getY(), nthPoint.getX(), nthPoint.getY());
-			
+				Point2D.distance(myPoint.getX(), myPoint.getY(), nthPoint.getX(), nthPoint.getY());
+
 			double f = -CONSTANT_SPRING * ((distance / ( 80 * 2)) - 1.0);
 
 			double dx = myPoint.getX() - nthPoint.getX();
@@ -67,30 +62,44 @@ public class NodeFunction {
 			double ddy = f * dy;
 			node.moveDelta(ddx, ddy);
 			nthNode.moveDelta(-ddx, -ddy);
-			
+
 		}
 	}
 	
-
 	/**
-	 * ばねモデルの計算(Membrane用)
-	 *
+	 * 引力の計算
+	 * <p>
+	 * このメソッドを呼び出すときはsynchronized (nodeMap_) を行うこと。
+	 * @param node
 	 */
 	static
-	private void calcSpring_Membrane(){
-//		Membrane mem = (Membrane)myObject;
-//		Point2D myPoint = getCenterPoint();
-//		for(int i = 0; i < atom.getEdgeCount() ; i++){
-//			Point2D nthPoint = LinkSet.getNodePoint(atom.getNthAtom(i));
-//			if(null == nthPoint){ continue; }
-//			
-//			double distance =
-//		    Point2D.distance(myPoint.getX(), myPoint.getY(), nthPoint.getX(), nthPoint.getY());
-//			
-//			double f = -CONSTANT_SPRING * (distance - 1.0);
-//			
-//			moveDelta((myPoint.getX() - nthPoint.getX()) * f, (myPoint.getY() - nthPoint.getY()) * f);
-//			
-//		}
+	public void calcAttraction(Node node, Map nodeMap){
+		if(!(node.getObject() instanceof Membrane) ||
+				null != node.getInvisibleRootNode())
+		{
+			return;
+		}
+		
+		Point2D myPoint = node.getCenterPoint();
+
+		Iterator<Node> nodes = nodeMap.values().iterator();
+		while(nodes.hasNext()){
+			// 表示されているNodeを取得する
+			Node targetNode = nodes.next();
+			Point2D nthPoint = targetNode.getCenterPoint();
+
+			double distance =
+				Point2D.distance(myPoint.getX(), myPoint.getY(), nthPoint.getX(), nthPoint.getY());
+
+			double f = -CONSTANT_SPRING * ((distance / 5) - 1.0);
+
+			double dx = myPoint.getX() - nthPoint.getX();
+			double dy = myPoint.getY() - nthPoint.getY();
+
+			double ddx = f * dx;
+			double ddy = f * dy;
+			System.out.println("move:"+ddx);
+			targetNode.moveDelta(-ddx, -ddy);
+		}
 	}
 }
