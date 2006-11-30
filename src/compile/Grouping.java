dd8493323d66e4bda5ed13d68e1cf6171067a0fa
sakurai <sakurai@ueda.info.waseda.ac.jp>
2006-11-30 15:10:17 +0000

    *** empty log message ***

--- src/compile/Grouping.java
+++ src/compile/Grouping.java
@@ -57,6 +57,7 @@ public class Grouping {
 	public void groupingInsts(List insts, boolean isAtomMatch){
 		if(((Instruction)insts.get(0)).getKind() != Instruction.SPEC) return;
 		spec = (Instruction)insts.get(0);
+		int last = -1;
 		for(int i=1; i<insts.size(); i++){
 			Instruction inst = (Instruction)insts.get(i);
 			//否定条件がある場合はグループ化は無効 暫定的処置
@@ -70,6 +71,17 @@ public class Grouping {
 			//System.out.println("instruction = "+inst);
 			if(inst.getOutputType() != -1)
 				var2DefInst.put(inst.getArg1(), inst);
+			last = i+1;
+		}
+		if(isAtomMatch){
+			for(int i=1; i<insts.size(); i++){
+				Instruction inst = (Instruction)insts.get(i);
+				if(inst.getKind() == Instruction.FUNC){
+					Inst2GroupId.put(inst, new Integer(last));
+					var2DefInst.put(inst.getArg1(), inst);
+					break;
+				}
+			}
 		}
 		//viewMap();
 		createGroup(insts, isAtomMatch);
@@ -109,7 +121,7 @@ public class Grouping {
 			if(list.isEmpty()) continue;
 			for (int j = 0; j < list.size(); j++) {
 				if (list.get(j).equals(new Integer(0))) continue;
-				if (list.get(j).equals(new Integer(1)) && isAtomMatch) continue;
+				//if (list.get(j).equals(new Integer(1)) && isAtomMatch) continue;
 				group = Inst2GroupId.get(var2DefInst.get(list.get(j)));
 				changegroup = Inst2GroupId.get(inst);
 				changeMap(changegroup, group);
@@ -121,24 +133,74 @@ public class Grouping {
 				|| inst.getKind() == Instruction.JUMP) break;
 			Object group = null;
 			Object changegroup = null;
+			boolean norules = false;
+			int natoms = -1;
+			int nmems = -1;
 			if(inst.getKind() == Instruction.ANYMEM
 					  || inst.getKind() == Instruction.LOCKMEM){
-						//膜を取得する命令は全て同じグループに属すことにする
-						//これも暫定的措置
-						//{a}, {b, $p}, {c} ･･･これらは実際は違うグループ
-						//{a(X)}, {a(Y)} ･･･これらは同じグループ
+						//膜をグループ分け
+						//ルールの有無による区別 {a, $p, @p} と {a, $p}は違うグループ
+						//アトム, 子膜の数による区別 {a, b} と {a, a, b, b} は違うグループ
+						//今のところ次の場合は区別できないので同じグループとする
+						//{a(X), b(X)}, {a(Y)}, b(Y) ･･･これらは違うグループ
+						//$in, $out の数で区別可能?
 						//todo どう区別するか考える
-						for(int i2 = 1; i2 < insts.size(); i2++){
+						for(int i2 = i+1; i2 < insts.size(); i2++){
 							Instruction inst2 = (Instruction)insts.get(i2);
+							switch(inst2.getKind()){
+							case Instruction.COMMIT:
+							case Instruction.JUMP:
+								break;
+							case Instruction.NORULES:
+								if((inst2.getArg1()).equals(inst.getArg1())) norules = true;
+								break;
+							case Instruction.NATOMS:
+								if((inst2.getArg1()).equals(inst.getArg1())) natoms = inst2.getIntArg2();
+								break;
+							case Instruction.NMEMS:
+								if((inst2.getArg1()).equals(inst.getArg1())) nmems = inst2.getIntArg2();
+								break;
+							default : break;
+							}
+						}
+						for(int j = i+1; j < insts.size(); j++){
+							Instruction inst2 = (Instruction)insts.get(j);
 							switch(inst2.getKind()){
 							case Instruction.COMMIT:
 							case Instruction.JUMP:
 								break;
 							case Instruction.ANYMEM:
 							case Instruction.LOCKMEM:
-								group = Inst2GroupId.get(inst);
-								changegroup = Inst2GroupId.get(inst2);
-								changeMap(changegroup, group);
+								boolean eqgroup = true;
+								boolean norules2 = false;
+								int natoms2 = -1;
+								int nmems2 = -1;
+								for(int j2 = j+1; j2 < insts.size(); j2++){
+									Instruction inst3 = (Instruction)insts.get(j2);
+									switch(inst3.getKind()){
+									case Instruction.COMMIT:
+									case Instruction.JUMP:
+										break;
+									case Instruction.NORULES:
+										if((inst3.getArg1()).equals(inst2.getArg1())) norules2 = true;
+										break;
+									case Instruction.NATOMS:
+										if((inst3.getArg1()).equals(inst2.getArg1())) natoms2 = inst3.getIntArg2();
+										break;
+									case Instruction.NMEMS:
+										if((inst3.getArg1()).equals(inst2.getArg1())) nmems2 = inst3.getIntArg2();
+										break;
+									default : break;
+									}
+								}
+								if(norules != norules2) eqgroup = false;
+								if(natoms != natoms2) eqgroup = false;
+								if(nmems != nmems2) eqgroup = false;
+								if(eqgroup){
+									group = Inst2GroupId.get(inst);
+									changegroup = Inst2GroupId.get(inst2);
+									changeMap(changegroup, group);
+								}
 								break;
 							default : break;
 							}
@@ -191,9 +253,19 @@ public class Grouping {
 //			System.out.println("");
 			group2Cost.put(groupinst, cost);
 		}
-//		Group命令の並び替え 膜主導テストのみ
-		if(!isAtomMatch)
-		for(int i=0; i<insts.size(); i++){
+//		Group命令の並び替え
+		//アトム主導テストは最初のfunc命令を含む命令の位置は先頭のままとする
+		int groupstart = 0;
+		if(isAtomMatch){
+			for(int i=0; i<insts.size(); i++){
+				Instruction inst = (Instruction)insts.get(i);
+				if(inst.getKind() == Instruction.GROUP){
+					groupstart = i+1;
+					break;
+				}
+			}
+		}
+		for(int i=groupstart; i<insts.size(); i++){
 			Instruction inst = (Instruction)insts.get(i);
 			Cost cost1 = null;
 			if(inst.getKind() == Instruction.GROUP){
