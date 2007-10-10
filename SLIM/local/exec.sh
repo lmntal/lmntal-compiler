eab9a806108e58e5bc9565d4c0d9748fe5c8f88a
riki <riki@ueda.info.waseda.ac.jp>
2007-10-10 10:16:44 +0000

    *** empty log message ***

--- SLIM/local/exec.sh
+++ SLIM/local/exec.sh
@@ -9,10 +9,13 @@ do
   [ $i -nt ${i%.*}.jil ] && $LMNHOME/bin/lmntal --compileonly --slimcode $i >${i%.*}.jil
   [ $i -nt ${i%.*}.sil ] && $LMNHOME/SLIM/src/conv_il --v1 <${i%.*}.jil >${i%.*}.sil
 
-  [ $i -nt ${i%.*}.jout ] && $LMNHOME/bin/lmntal $i >${i%.*}.jout
+  [ $i -nt ${i%.*}.jout ] && $LMNHOME/bin/lmntal --hideruleset --hiderule $i >${i%.*}.jout
   ($LMNHOME/SLIM/src/slim --hideruleset ${i%.*}.sil >${i%.*}.sout) >&${i%.*}.serr
 
-  if [ "`$LMNHOME/SLIM/src/slimtest.sh $i ${i%.*}.sout`" = "success" ];
+#  $LMNHOME/SLIM/src/slimtest.sh $i ${i%.*}.sout >${i%.*}.check
+  $LMNHOME/SLIM/local/slimtest.sh ${i%.*}.jout ${i%.*}.sout >${i%.*}.check
+
+  if [ "`cat ${i%.*}.check`" = "success" ];
   then
       echo "$i is success"
   else
