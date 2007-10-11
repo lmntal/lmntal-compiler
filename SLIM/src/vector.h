8e7b40478736e77b566157e7cbe4a01c0839cace
okabe <okabe@ueda.info.waseda.ac.jp>
2007-10-11 07:21:06 +0000

    refactor

--- SLIM/src/vector.h
+++ SLIM/src/vector.h
@@ -1,6 +1,5 @@
-/* 
- * Vector <LmnWord>
- * キーをアドレス値とすることで汎用的なデータ構造を扱うことができる。
+/*
+ * Vector<LmnWord>
  */
 
 #ifndef LMN_VECTOR_H
@@ -14,11 +13,10 @@ typedef struct Vector {
 } Vector;
 
 LMN_EXTERN Vector *vec_init(Vector *vec, unsigned int init_size);
-LMN_EXTERN Vector* vec_make(unsigned int init_size);
-LMN_EXTERN Vector* vec_make_default(void);
-LMN_EXTERN void vec_push(Vector* vec, LmnWord keyp);
+LMN_EXTERN Vector *vec_make(unsigned int init_size);
+LMN_EXTERN void vec_push(Vector *vec, LmnWord keyp);
 LMN_EXTERN LmnWord vec_pop(Vector *vec);
-LMN_EXTERN void vec_set(Vector* vec, unsigned int index, LmnWord keyp);
+LMN_EXTERN void vec_set(Vector *vec, unsigned int index, LmnWord keyp);
 LMN_EXTERN LmnWord vec_get(Vector *vec, unsigned int index);
 LMN_EXTERN int vec_indexof(Vector *vec, LmnWord keyp);
 LMN_EXTERN void vec_clear(Vector *vec);
