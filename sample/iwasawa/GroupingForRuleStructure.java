f54bd4a40fae973d9a71bed7fadb70505eabdcf3
iwasawa <iwasawa@ueda.info.waseda.ac.jp>
2008-11-11 03:51:36 +0000

    *** empty log message ***

---
+++ sample/iwasawa/GroupingForRuleStructure.java
@@ -0,0 +1,13 @@
+package compile;
+
+import java.util.List;
+
+import compile.structure.Atomic;
+import compile.structure.Membrane;
+
+class GroupingForRuleStructure {
+	List<Atomic> atomList;
+	List<Membrane> memList;
+	//List<ProcessContext> pcList;
+	//List<>
+}
