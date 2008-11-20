120e175c93a16c9220c92b1d6082bd3fd4c8d7e9
iwasawa <iwasawa@ueda.info.waseda.ac.jp>
2008-11-20 17:02:17 +0000

    *** empty log message ***

--- sample/iwasawa/GroupingForRuleStructure.java
+++ sample/iwasawa/GroupingForRuleStructure.java
@@ -6,6 +6,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
+import runtime.Functor;
+
 import util.Util;
 
 import compile.structure.Atom;
@@ -70,6 +72,24 @@ class GroupingForRuleStructure {
 		processContextGroupList.add(new LinkedList<ProcessContext>());
 	}
 
+	List<List> getatommemsList(){
+		List<List> groupList = new LinkedList<List>();
+		for(int i=0; i<atomGroupList.size(); i++){
+			List group = new LinkedList();
+			for(Atom atom : atomGroupList.get(i)){
+				if(isInGlobalRootMem(atom) && ! (atom.functor.getName().equals(Functor.UNIFY.getName())))
+					group.add(atom);
+			}
+			for(Object mem : memGroupList.get(i)){
+				if(isInGlobalRootMem(mem)){
+					group.add(mem);
+					
+				}
+			}
+			groupList.add(group);
+		}
+		return groupList;
+	}
 	void makeGroup(){
 		atomGroupList = new ArrayList<List<Atom>>();
 		memGroupList = new ArrayList<List<Membrane>>();
@@ -86,6 +106,7 @@ class GroupingForRuleStructure {
 			if(visitedAtoms.contains(atom)) continue;
 			visitedAtoms.add(atom);
 			addGroupList();
+			atomGroupList.get(atomGroupList.size()-1).add(atom);
 			makeGroupAtomic(atomLink);
 		}
 		for(AtomicLink<Membrane> memLink : memLinks){
@@ -93,6 +114,7 @@ class GroupingForRuleStructure {
 			if(visitedMembranes.contains(m)) continue;
 			visitedMembranes.add(m);
 			addGroupList();
+			memGroupList.get(memGroupList.size()-1).add(m);
 			makeGroupAtomic(memLink);
 		}
 		for(AtomicLink<ProcessContext> tpcLink : typedProcessContextLinks){
@@ -100,6 +122,7 @@ class GroupingForRuleStructure {
 			if(visitedTypedProcessContexts.contains(tpc)) continue;
 			visitedTypedProcessContexts.contains(tpc);
 			addGroupList();
+			typedProcessContextGroupList.get(typedProcessContextGroupList.size()-1).add(tpc);
 			makeGroupAtomic(tpcLink);
 		}
 		for(AtomicLink<ProcessContext> pcLink : processContextLinks){
@@ -107,6 +130,7 @@ class GroupingForRuleStructure {
 			if(visitedProcessContexts.contains(pc)) continue;
 			visitedProcessContexts.add(pc);
 			addGroupList();
+			processContextGroupList.get(processContextGroupList.size()-1).add(pc);
 			makeGroupAtomic(pcLink);
 		}
 	}
@@ -116,27 +140,33 @@ class GroupingForRuleStructure {
 	
 	private <E> void makeGroupAtomic(AtomicLink<E> atomicLink){
 		for(Atom a : atomicLink.atomList){
+			if(visitedAtoms.contains(a)) continue;
+			visitedAtoms.add(a);
 			atomGroupList.get(atomGroupList.size()-1).add(a);
-			makeGroupAtomic(a, visitedAtoms, atomLinks);
+			makeGroupAtomic(a, atomLinks);
 		}
 		for(Membrane m : atomicLink.memList){
+			if(visitedMembranes.contains(m)) continue;
+			visitedMembranes.add(m);
 			memGroupList.get(memGroupList.size()-1).add(m);
-			makeGroupAtomic(m, visitedMembranes, memLinks);
+			makeGroupAtomic(m, memLinks);
 		}
 		for(ProcessContext tpc : atomicLink.typedProcessContextList){
+			if(visitedTypedProcessContexts.contains(tpc)) continue;
+			visitedTypedProcessContexts.add(tpc);
 			typedProcessContextGroupList.get(typedProcessContextGroupList.size()-1).add(tpc);
-			makeGroupAtomic(tpc, visitedTypedProcessContexts, typedProcessContextLinks);
+			makeGroupAtomic(tpc, typedProcessContextLinks);
 		}
 		for(ProcessContext pc : atomicLink.processContextList){
+			if(visitedProcessContexts.contains(pc)) continue;
+			visitedProcessContexts.add(pc);
 			processContextGroupList.get(processContextGroupList.size()-1).add(pc);
-			makeGroupAtomic(pc, visitedProcessContexts, processContextLinks);
+			makeGroupAtomic(pc, processContextLinks);
 		}
 
 	}
 	
-	private <E> void makeGroupAtomic(E atomic, Set<E> visited, List<AtomicLink<E>> atomicLinks){
-		if(visited.contains(atomic)) return;
-		visited.add(atomic);
+	private <E> void makeGroupAtomic(E atomic, List<AtomicLink<E>> atomicLinks ){
 		makeGroupAtomic(get(atomicLinks, atomic));
 	}
 
@@ -161,27 +191,42 @@ class GroupingForRuleStructure {
 		for(AtomicLink<E> atomicLink : atomicLinkList){
 			if(atomicLink.atomic == e) return atomicLink;
 		}
+		Util.println("\n\n");
+		Util.println(atomicLinkList);
+		Util.println(e);
 		throw new RuntimeException();
+	}
+	
+	private boolean isFreeLink(LinkOccurrence lo){
+		return rs.leftMem.freeLinks.containsValue(lo);
+	}
+	private boolean isInGlobalRootMem(Object atomic){
+		if(atomic instanceof Atom){
+			return ((Atom)atomic).mem.parent == null;
+		}else if(atomic instanceof Membrane){
+			return ((Membrane)atomic).parent == rs.leftMem;
+		}else if(atomic instanceof ProcessContext){
+			return ((ProcessContext)atomic).mem.parent == null;
+		}else{
+			throw new RuntimeException();
+		}
 	}
 	private void makeLinkGraphForAtom(){
 		for(AtomicLink<Atom> atomLink : atomLinks){
 			Atom atom = atomLink.atomic;
 
 			if(has(memLinks,  atom.mem)){
-				atomLink.memList.add(atom.mem);
+				atomLink.addMembraneListUniq(atom.mem);
 			}
 			for(LinkOccurrence otherSide : atom.args){
+				if(isFreeLink(otherSide)) continue;
 				Atomic a = otherSide.buddy.atom;
-				if(a == null) continue;
-				if(a instanceof Atom) atomLink.atomList.add((Atom)a);
+				
+				if(a instanceof Atom) atomLink.addAtomListUniq((Atom)a);
 				else if(a instanceof ProcessContext){
-					if(rs.leftMem.typedProcessContexts.contains(a)){
-						atomLink.typedProcessContextList.add((ProcessContext)a);
-					}else if(rs.leftMem.processContexts.contains(a)){
-						atomLink.processContextList.add((ProcessContext)a);
-					}else{
-						throw new RuntimeException();
-					}
+					ProcessContext pc = ((ProcessContext)a);
+					if(pc.def.typed) atomLink.addTypedProcessContextListUniq(pc);
+					else atomLink.addProcessContextListUniq(pc);
 				}
 			}
 		}
@@ -191,20 +236,20 @@ class GroupingForRuleStructure {
 		for(AtomicLink<Membrane> memLink : memLinks){
 			Membrane mem = memLink.atomic;
 			for(Atom a : mem.atoms){
-				memLink.atomList.add(a);
+				memLink.addAtomListUniq(a);
 			}
 
-			if(has(memLinks, mem.parent)) memLink.memList.add(mem.parent);
+			if(has(memLinks, mem.parent)) memLink.addMembraneListUniq(mem.parent);
 			for(Membrane subMem : mem.mems){
-				memLink.memList.add(subMem);
+				memLink.addMembraneListUniq(subMem);
 			}
 
 			for(ProcessContext tpc : mem.typedProcessContexts){
-				memLink.typedProcessContextList.add(tpc);
+				memLink.addTypedProcessContextListUniq(tpc);
 			}
 
 			for(ProcessContext pc : mem.processContexts){
-				memLink.processContextList.add(pc);
+				memLink.addProcessContextListUniq(pc);
 			}
 		}
 	}
@@ -213,33 +258,31 @@ class GroupingForRuleStructure {
 		for(AtomicLink<ProcessContext> tpcLink : typedProcessContextLinks){
 			ProcessContext tpc = tpcLink.atomic;
 
+			/* globalルート膜に所属している型付プロセス文脈は所属膜とリンクしない。 */
 			if(has(memLinks,  tpc.mem))
-				tpcLink.memList.add(tpc.mem);
+				tpcLink.addMembraneListUniq(tpc.mem);
 
 			for(LinkOccurrence otherSide : tpc.args){
+				if(isFreeLink(otherSide)) continue;
 				Atomic a = otherSide.buddy.atom;
-				if(a == null) continue;
-				if(a instanceof Atom) tpcLink.atomList.add((Atom)a);
+	
+				if(a instanceof Atom) tpcLink.addAtomListUniq((Atom)a);
 				else if(a instanceof ProcessContext){
-					if(rs.leftMem.typedProcessContexts.contains(a)){
-						tpcLink.typedProcessContextList.add((ProcessContext)a);
-					}else if(rs.leftMem.processContexts.contains(a)){
-						tpcLink.processContextList.add((ProcessContext)a);
-					}else{
-						throw new RuntimeException();
-					}
+					ProcessContext pc = (ProcessContext)a;
+					if(pc.def.typed) tpcLink.addTypedProcessContextListUniq(pc);
+					else tpcLink.addProcessContextListUniq(pc);
 				}
 			}
 
 			for(AtomicLink<ProcessContext> tpcLink2 : typedProcessContextLinks){
 				ProcessContext tpc2 = tpcLink2.atomic;
 				if(tpc == tpc2) continue;
-				tpcLink.typedProcessContextList.add(tpc2);
+				tpcLink.addTypedProcessContextListUniq(tpc2);
 			}
 
 			for(AtomicLink<ProcessContext> pcLink : processContextLinks){
 				ProcessContext pc = pcLink.atomic;
-				tpcLink.processContextList.add(pc);
+				tpcLink.addProcessContextListUniq(pc);
 			}
 		}
 	}
@@ -249,34 +292,30 @@ class GroupingForRuleStructure {
 			ProcessContext pc = pcLink.atomic;
 
 			if(has(memLinks, pc.mem))
-				pcLink.memList.add(pc.mem);
+				pcLink.addMembraneListUniq(pc.mem);
 
 			for(LinkOccurrence otherSide : pc.args){
+				if(isFreeLink(otherSide)) continue;
 				Atomic a = otherSide.buddy.atom;
-				if(a == null) continue;
-				if(a instanceof Atom) pcLink.atomList.add((Atom)a);
+
+				if(a instanceof Atom) pcLink.addAtomListUniq((Atom)a);
 				else if(a instanceof ProcessContext){
-					if(rs.leftMem.typedProcessContexts.contains(a)){
-						pcLink.typedProcessContextList.add((ProcessContext)a);
-					}else if(rs.leftMem.processContexts.contains(a)){
-						pcLink.processContextList.add((ProcessContext)a);
-					}else{
-						throw new RuntimeException();
-					}
+					ProcessContext pc2 = (ProcessContext)a;
+					if(pc2.def.typed) pcLink.addTypedProcessContextListUniq(pc2);
+					else pcLink.addProcessContextListUniq(pc2);
 				}
 			}
 
 			for(AtomicLink<ProcessContext> tpcLink : typedProcessContextLinks){
 				ProcessContext tpc = tpcLink.atomic;
-				pcLink.typedProcessContextList.add(tpc);
+				pcLink.addTypedProcessContextListUniq(tpc);
 			}
 
 			for(AtomicLink<ProcessContext> pcLink2 : processContextLinks){
 				ProcessContext pc2 = pcLink2.atomic;
 				if(pc == pc2) continue;
-				pcLink.processContextList.add(pc2);
+				pcLink.addProcessContextListUniq(pc2);
 			}
-
 		}
 	}
 
@@ -321,18 +360,35 @@ class GroupingForRuleStructure {
 			memList = new LinkedList<Membrane>();
 			processContextList = new LinkedList<ProcessContext>();
 			typedProcessContextList = new LinkedList<ProcessContext>();
-
 		}
+		
 
+		
+		void addAtomListUniq(Atom atom){
+			if(atomList.contains(atom)) return;
+			atomList.add(atom);
+		}
+		void addMembraneListUniq(Membrane mem){
+			if(memList.contains(mem)) return;
+			memList.add(mem);
+		}
+		void addTypedProcessContextListUniq(ProcessContext tpc){
+			if(typedProcessContextList.contains(tpc)) return;
+			typedProcessContextList.add(tpc);
+		}
+		void addProcessContextListUniq(ProcessContext pc){
+			if(processContextList.contains(pc)) return;
+			processContextList.add(pc);
+		}
 		public String toString(){
 			StringBuffer sb = new StringBuffer();
+			sb.append("\n");
 			sb.append(atomic);
 			sb.append(atomList);
 			sb.append(memList);
 			sb.append(typedProcessContextList);
 			sb.append(processContextList);
 			return sb.toString();
-
 		}
 	}
 
