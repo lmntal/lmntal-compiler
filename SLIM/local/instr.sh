11e2796cd23dee823f847fba7bd3ffd21522bff7
riki <riki@ueda.info.waseda.ac.jp>
2008-12-10 07:30:16 +0000

    delete old SLIM

--- SLIM/local/instr.sh
+++
@@ -1,6 +0,0 @@
-# /bin/sh
-
-LMNHOME=~/workspace/lmntal
-LINE=`expr $1 + 5`
-
-sed --quiet -e "${LINE}p" $LMNHOME/SLIM/src/instruction.h
--- SLIM/src/read_symtbl.h
+++
@@ -1,7 +0,0 @@
-/*
- * read_symtbl.h
- */
-
-void read_quoted_str(FILE* file, char *buf, size_t *buf_size, unsigned int *str_len);
-void read_int(FILE* file, unsigned int *n);
-void load_symbol(char *file_name);
