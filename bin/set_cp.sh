b7064c8806f4839e2499505d1964003b6643b841
nikosai <nikosai2531@gmail.com>
2020-02-11 00:14:39 +0900

    fix set_cp.sh

--- bin/set_cp.sh
+++ bin/set_cp.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-export LMNTAL_HOME="$(dirname $(realpath "$0"))/.."
+if [ -L "$0" ]; then
+    export LMNTAL_HOME="$(dirname $(readlink "$0"))/.."
+else 
+    export LMNTAL_HOME="$(dirname "$0")/.."
+fi
 
 CLASSPATH="$LMNTAL_HOME/classes"
 for f in "$LMNTAL_HOME"/lib/*.jar; do CLASSPATH="$CLASSPATH:$f"; done
