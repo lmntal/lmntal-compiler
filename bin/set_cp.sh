c604bb529c6e6ca6af11299251cc4af95431dfb2
nikosai <nikosai2531@gmail.com>
2020-02-11 23:45:48 +0900

    add double-quotes

--- bin/set_cp.sh
+++ bin/set_cp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ -L "$0" ]; then
-    CWD=$(dirname $(readlink "$0"))
+    CWD="$(dirname $(readlink "$0"))"
 else
-    CWD=$(dirname "$0")
+    CWD="$(dirname "$0")"
 fi
-export LMNTAL_HOME="$(cd $CWD/.. && pwd)"
+export LMNTAL_HOME="$(cd "$CWD"/.. && pwd)"
 
 CLASSPATH="$LMNTAL_HOME/classes"
 for f in "$LMNTAL_HOME"/lib/*.jar; do CLASSPATH="$CLASSPATH:$f"; done
