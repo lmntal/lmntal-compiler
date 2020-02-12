9de58365653bdc3cd98d8707a3baf8326528ed8d
nikosai <nikosai2531@gmail.com>
2020-02-12 14:25:57 +0900

    simplify the solution

--- bin/set_cp.sh
+++ bin/set_cp.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ -L "$0" ]; then
-    CWD="$(cd $(dirname "$0") && cd $(dirname $(readlink "$0")) && pwd)"
-else
-    CWD="$(dirname "$0")"
-fi
-export LMNTAL_HOME="$(cd "$CWD"/.. && pwd)"
+export LMNTAL_HOME="$(cd "$(dirname "$0")"/.. && pwd)"
 
 CLASSPATH="$LMNTAL_HOME/classes"
 for f in "$LMNTAL_HOME"/lib/*.jar; do CLASSPATH="$CLASSPATH:$f"; done
